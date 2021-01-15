c2d8d60895fbef07811ce22e36936c7af68906dd
Aadhiya <aadhiyamaria@gmail.com>
2021-01-15 23:20:07 +0530

    Update Pig.js

--- p5.play-boilerplate-master/Pig.js
+++ p5.play-boilerplate-master/Pig.js
@@ -1,4 +1,4 @@
-class Pig {
+/*class Pig {
     constructor(x, y) {
       var options = {
           'restitution':0.8,
@@ -19,6 +19,33 @@ class Pig {
       rotate(angle);
       rectMode(CENTER);
       fill("pink");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };*/
+
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+
+      fill("green");
       rect(0, 0, this.width, this.height);
       pop();
     }
